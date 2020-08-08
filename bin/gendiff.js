7dec026dd895f52aef8ede8ae3cd7db40db75d95
MYiLA <polus180@ya.ru>
2020-08-08 18:27:31 +0300

    add arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import commander from 'commander';
 
+const file1 = {};
+const file2 = {};
+
 commander
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
@@ -8,6 +11,13 @@ commander
   .option('-f, --format [type]', 'output format')
 
 commander.parse(process.argv)
+
+//получила аргументы
+console.log(`'Я БИБЛИОТЕКА' ${process.argv.slice(2)}`);
+
+// нашла нужные файлы по названию
+// спарсила json в объекты с данными
+// сравнила данные и собрала из них строчку для вывода
 
 const compare = (filepath1, filepath12) => {
   
