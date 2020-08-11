4d07fb554aadbfec51332dda627f93e4516bd3ee
MYiLA <polus180@ya.ru>
2020-08-12 01:53:19 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,8 @@
-// import index from '../index.js';
-
-// test('test diff json file', () => {
-//   expect(index('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(
-//     '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}',
-//   );
-// });
-
-// const sum = require('../src/sum.js');
 import { test, expect } from '@jest/globals';
-import sum from '../src/sum.js';
+import index from '../index.js';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('test diff json file', () => {
+  expect(index('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(
+     '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}',
+    );
 });
