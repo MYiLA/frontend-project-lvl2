2098c233fa9427ec40f66aeeb63db803ae5e7649
MYiLA <polus180@ya.ru>
2020-08-13 03:01:11 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,7 @@
-import { test, expect } from '@jest/globals';
 import index from '../index.js';
 
 test('test diff json file', () => {
   expect(index('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(
-     '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}',
-    );
+    '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}',
+  );
 });
